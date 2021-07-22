8048622ece5b0da545c08f330aebdc42f9cdd51d
Susana Reyes <60724497+susi1603@users.noreply.github.com>
2021-07-21 22:39:29 -0500

    bulb added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import HighlightIcon from "@material-ui/icons/Highlight";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
 
 function Header() {
   return (
     <header>
       <h1>
-        <HighlightIcon />
+        <EmojiObjectsIcon />
         Keeper
       </h1>
     </header>
